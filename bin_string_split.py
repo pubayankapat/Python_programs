70e68617f303ffa9effb79bed338c39395313cb0
pubayankapat <pubayankapat@gmail.com>
2025-10-12 23:56:44 +0530

    updating

--- bin_string_split.py
+++ bin_string_split.py
@@ -1,1 +1,17 @@
-givenStr = int(input())
+givenStr = input()
+
+countOne = 0
+countZero = 0
+
+for i in givenStr:
+    if i == 0:
+        countZero += 1
+    elif i == 1:
+        countOne += 1
+
+if countOne % 2 == 1 or countZero % 2 == 1:
+    print("-1")
+else:
+    currZero = 0;
+    currOne = 0;
+
